e5a0684b5cc3d13f7aac63754a1b0fe600496b76
knir4 <knir4@gmail.com>
2024-01-30 16:30:54 +0200

    first commit

---
+++ app/src/main/java/com/example/recycle/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.recycle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private LinearLayoutManager layoutManager;
+
+    private ArrayList<DataModel> dataSet;
+
+    private CustomAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.res);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        dataSet = new ArrayList<>();
+
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+
+        for(int i = 0 ; i < MyData.drawableArray.length ; i++){
+            dataSet.add(new DataModel(
+                    MyData.nameArray[i],
+                    MyData.versionArray[i],
+                    MyData.id_[i],
+                    MyData.drawableArray[i]
+            ));
+        }
+
+        adapter = new CustomAdapter(dataSet);
+        recyclerView.setAdapter(adapter);
+
+    }
+}
